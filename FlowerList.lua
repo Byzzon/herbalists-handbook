d6b4c59dc85f6276513b428d39e4a036093b682f
Gilian Hoskens <gilianhoskens@gmail.com>
2025-03-16 12:11:51 +0100

    Update flowerlist, so flowers only have to be added once.

--- FlowerList.lua
+++ FlowerList.lua
@@ -1,94 +1,114 @@
 ByzzonUI = ByzzonUI or {}
 
-local peacebloom = {
-    iconPath = [[Interface\icons\Inv_misc_flower_02]],
-    name = "Peacebloom",
-    orangeLevel = 1,
-    yellowLevel = 25,
-    greenLevel = 50,
-    grayLevel = 100,
-    locations = "All low level zones"
+ByzzonUI.flowerList = {
+    {
+        iconPath = [[Interface\icons\Inv_misc_flower_02]],
+        name = "Peacebloom",
+        orangeLevel = 1,
+        yellowLevel = 25,
+        greenLevel = 50,
+        grayLevel = 100,
+        locations = "All low level zones"
+    },
+    {
+        iconPath = [[Interface\icons\Inv_misc_herb_10]],
+        name = "Silverleaf",
+        orangeLevel = 1,
+        yellowLevel = 25,
+        greenLevel = 50,
+        grayLevel = 100,
+        locations = "All low level zones"
+    },
+    {
+        iconPath = [[Interface\icons\Inv_misc_herb_07]],
+        name = "Earthroot",
+        orangeLevel = 15,
+        yellowLevel = 40,
+        greenLevel = 70,
+        grayLevel = 115,
+        locations = "All low level zones"
+    },
+    {
+        iconPath = [[Interface\icons\inv_jewelry_talisman_03]],
+        name = "Mageroyal",
+        orangeLevel = 50,
+        yellowLevel = 75,
+        greenLevel = 100,
+        grayLevel = 150,
+        locations = "All low level zones"
+    },
+    {
+        iconPath = [[Interface\icons\inv_misc_root_01]],
+        name = "Briarthorn",
+        orangeLevel = 70,
+        yellowLevel = 95,
+        greenLevel = 120,
+        grayLevel = 170,
+        locations = "All low level zones, especially Silverpine Forest"
+    },
+    {
+        iconPath = [[Interface\icons\inv_misc_herb_04]],
+        name = "Swiftthistle",
+        orangeLevel = "",
+        yellowLevel = "",
+        greenLevel = "",
+        grayLevel = "",
+        locations = "All low level zones, found on [Briarthorn] and [Mageroyal]"
+    },
+    {
+        iconPath = [[Interface\icons\inv_misc_herb_11]],
+        name = "Stranglekelp",
+        orangeLevel = 85,
+        yellowLevel = 115,
+        greenLevel = 135,
+        grayLevel = 185,
+        locations = "In the ocean, especially near Menethil Harbour and Zoram Strand"
+    },
+    {
+        iconPath = [[Interface\icons\inv_misc_herb_01]],
+        name = "Bruiseweed",
+        orangeLevel = 100,
+        yellowLevel = 130,
+        greenLevel = 150,
+        grayLevel = 200,
+        locations = "Near Houses in low-mid level zones"
+    },
+    {
+        iconPath = [[Interface\icons\inv_misc_flower_01]],
+        name = "Wild Steelbloom",
+        orangeLevel = 115,
+        yellowLevel = 145,
+        greenLevel = 165,
+        grayLevel = 215,
+        locations = "All low-mid level zones"
+    },
+    {
+        iconPath = [[Interface\icons\inv_misc_dust_02]],
+        name = "Grave Moss",
+        orangeLevel = 120,
+        yellowLevel = 150,
+        greenLevel = 170,
+        grayLevel = 220,
+        locations = "Very Rare, found only in Graveyards"
+    },
+    {
+        iconPath = [[Interface\icons\inv_misc_herb_03]],
+        name = "Kingsblood",
+        orangeLevel = 125,
+        yellowLevel = 155,
+        greenLevel = 175,
+        grayLevel = 225,
+        locations = "Mid level zones"
+    },
+    {
+        iconPath = [[Interface\icons\inv_misc_root_02]],
+        name = "Liferoot",
+        orangeLevel = 150,
+        yellowLevel = 180,
+        greenLevel = 200,
+        grayLevel = 250,
+        locations = "Stranglethorn Vale, Wetlands and Swamp of Sorrows, near pools of water"
+    }
 }
 
-local silverleaf = {
-    iconPath = [[Interface\icons\Inv_misc_herb_10]],
-    name = "Silverleaf",
-    orangeLevel = 1,
-    yellowLevel = 25,
-    greenLevel = 50,
-    grayLevel = 100,
-    locations = "All low level zones"
-}
-
-local earthroot = {
-    iconPath = [[Interface\icons\Inv_misc_herb_07]],
-    name = "Earthroot",
-    orangeLevel = 15,
-    yellowLevel = 40,
-    greenLevel = 70,
-    grayLevel = 115,
-    locations = "All low level zones"
-}
-
-local mageroyal = {
-    iconPath = [[Interface\icons\inv_jewelry_talisman_03]],
-    name = "Mageroyal",
-    orangeLevel = 50,
-    yellowLevel = 75,
-    greenLevel = 100,
-    grayLevel = 150,
-    locations = "All low level zones"
-}
-
-local briarthorn = {
-    iconPath = [[Interface\icons\inv_misc_root_01]],
-    name = "Briarthorn",
-    orangeLevel = 70,
-    yellowLevel = 95,
-    greenLevel = 120,
-    grayLevel = 170,
-    locations = "All low level zones, especially Silverpine Forest"
-}
-
-local swiftthistle = {
-    iconPath = [[Interface\icons\inv_misc_herb_04]],
-    name = "Swiftthistle",
-    orangeLevel = "",
-    yellowLevel = "",
-    greenLevel = "",
-    grayLevel = "",
-    locations = "All low level zones, found on [Briarthorn] and [Mageroyal]"
-}
-
-local stranglekelp = {
-    iconPath = [[Interface\icons\inv_misc_herb_11]],
-    name = "Stranglekelp",
-    orangeLevel = 85,
-    yellowLevel = 115,
-    greenLevel = 135,
-    grayLevel = 185,
-    locations = "In the ocean, especially near Menethil Harbour and Zoram Strand"
-}
-
-local bruiseweed = {
-    iconPath = [[Interface\icons\inv_misc_herb_01]],
-    name = "Bruiseweed",
-    orangeLevel = 100,
-    yellowLevel = 130,
-    greenLevel = 150,
-    grayLevel = 200,
-    locations = "Near Houses in low-mid level zones"
-}
-
-local wild_steelbloom = {
-    iconPath = [[Interface\icons\inv_misc_flower_01]],
-    name = "Wild Steelbloom",
-    orangeLevel = 115,
-    yellowLevel = 145,
-    greenLevel = 165,
-    grayLevel = 215,
-    locations = "All low-mid level zones"
-}
-
-ByzzonUI.flowerList = { peacebloom, silverleaf, earthroot, mageroyal, briarthorn, swiftthistle, stranglekelp, bruiseweed,
-    wild_steelbloom }
+return ByzzonUI.flowerList;
